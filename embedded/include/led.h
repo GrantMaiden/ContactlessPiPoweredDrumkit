2d82556fadd24c121753f755d3cc9e88dcf9dc9f
peteroh770 <146861518+peteroh770@users.noreply.github.com>
2024-02-27 17:49:36 +0000

    Add files via upload
    state machine and unit test function added

--- embedded/include/led.h
+++ embedded/include/led.h
@@ -7,14 +7,28 @@ Description:    contains led definitions and headers
 
 // LED Colors
 #define LED_COLOR_WHITE_DIM         0x010101
+#define LED_COLOR_WHITE_MAX         0x111111
 #define LED_COLOR_RED_DIM           0x010000
 #define LED_COLOR_GREEN_DIM         0x000100
 #define LED_COLOR_BLUE_DIM          0x000001
 #define LED_COLOR_PURPLE_DIM        0x010001
 #define LED_COLOR_YELLOW_DIM        0x010100
 #define LED_COLOR_CYAN_DIM          0x000101
-#define LED_COLOR_ORANGE_BRIGHT     0xFE5A1D
+#define LED_COLOR_ORANGE_BRIGHT     0xCC8400
 #define LED_COLOR_OFF               0x000000
+
+// LED Speed
+#define INITIAL_SPEED               17
+
+enum TSM:int
+{
+    INITIAL1,
+    INITIAL2,
+    INITIAL3,
+    INITIAL4,
+    INITIAL5,
+    INITIAL6
+};
 
 /**
  * creates a single color array combined from input arguments. Will fill outputArr memory with combined data. Performs RGB->GRB byteshift
@@ -29,3 +43,7 @@ Description:    contains led definitions and headers
 void ledCreateColorArr(char* outputArr,unsigned led1, unsigned led2, unsigned led3, unsigned led4, unsigned led5, unsigned led6);
 
 
+/**
+ * Peter's LED test
+ **/
+void ledInitialiseTest();
