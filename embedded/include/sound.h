688ba0ad0af9ad5cf3fc3127af71c5c300e9e054
Grant Maiden <granttm@gmail.com>
2024-03-11 11:39:23 -0700

    Add Sounds to project
    soundTest3 is now defined.
    Sounds are enabled in project.
    Logic has been added to attach sounds to drums

--- embedded/include/sound.h
+++ embedded/include/sound.h
@@ -21,7 +21,6 @@ Description:    contains sound definitions and headers
 
 #ifdef EN_AUDIO
 
-//#include "context.h"
 //#define MA_NO_ENGINE
 //#define MINIAUDIO_IMPLEMENTATION
 #define MA_THREADCALL
@@ -33,12 +32,44 @@ extern "C"{
 #include "miniaudio.h"
 }
 
-#define TOTAL_INSTRUMENTS       3
-#define TH_LOUD_FOOT_CLOSED     "./sounds/grant_th_foot_closed_loud.wav"
-#define TH_LOUD_OPEN            "./sounds/grant_th_open_loud"
-#define DRUM3_LOUD              "./sounds/grant_metal_ting_loud.wav"
+#define CHANNELS    2               /* Must be stereo for this example. */
+#define SAMPLE_RATE 44100
 
+#define TOTAL_INSTRUMENTS               3
+#define TOTAL_INSTRUMENTS_SOUND_TEST2   3
 
+#define PATH_SOUND_ENABLED_WAV          "./sounds/sound_enabled.wav"
+#define PATH_SOUND_DISABLED_WAV         "./sounds/sound_disabled.wav"
+
+// Top Hat Sounds... Drums 1 and 2
+#define TH_LOW_OPEN                     "./sounds/grant_th_open_low.wav"
+#define TH_MEDIUM_OPEN                  "./sounds/grant_th_open_medium.wav"
+#define TH_LOUD_OPEN                    "./sounds/grant_th_open_loud.wav"
+#define TH_LOW_CLOSED                   "./sounds/grant_th_closed_low.wav"
+#define TH_MEDIUM_CLOSED                "./sounds/grant_th_closed_medium.wav"
+#define TH_LOUD_CLOSED                  "./sounds/grant_th_closed_loud.wav"
+#define TH_LOW_FOOT_OPEN                "./sounds/grant_th_foot_open_low.wav"
+#define TH_MEDIUM_FOOT_OPEN             "./sounds/grant_th_foot_open_medium.wav"
+#define TH_LOUD_FOOT_OPEN               "./sounds/grant_th_foot_open_loud.wav"
+#define TH_LOW_FOOT_CLOSED              "./sounds/grant_th_foot_closed_low.wav"
+#define TH_MEDIUM_FOOT_CLOSED           "./sounds/grant_th_foot_closed_medium.wav"
+#define TH_LOUD_FOOT_CLOSED             "./sounds/grant_th_foot_closed_loud.wav"
+
+#define DRUM3_LOW                       "./sounds/grant_metal_ting_low.wav"
+#define DRUM3_MEDIUM                    "./sounds/grant_metal_ting_medium.wav"
+#define DRUM3_LOUD                      "./sounds/grant_metal_ting_loud.wav"
+
+#define DRUM4_LOW                       "./sounds/grant_pop_low.wav"
+#define DRUM4_MEDIUM                    "./sounds/grant_pop_medium.wav"
+#define DRUM4_LOUD                      "./sounds/grant_pop_loud.wav"
+
+#define DRUM5_LOW                       "./sounds/grant_clap_low.wav"
+#define DRUM5_MEDIUM                    "./sounds/grant_clap_medium.wav"
+#define DRUM5_LOUD                      "./sounds/grant_clap_loud.wav"
+
+#define DRUM6_LOW                       "./sounds/grant_bass_low.wav"
+#define DRUM6_MEDIUM                    "./sounds/grant_bass_medium.wav"
+#define DRUM6_LOUD                      "./sounds/grant_bass_loud.wav"
 
 using namespace std;
 class Sound
@@ -63,14 +94,25 @@ class Sound
          * Tests sound engine
          **/
         void soundTest3();
+
+        /**
+         * Plays sound at a path. Sound must be preloaded in sound init.
+         * @param path - path of wave file to be played
+         **/
+        void playSound(const char * path);
     private:
 
         //char * mixArr = new char[TOTAL_INSTRUMENTS]();
         //char * mixArr[] = {"%s %s %s",TH_LOUD_FOOT_CLOSED, TH_LOUD_OPEN, DRUM3_LOUD};
 
         //static ma_thread_result custom_job_thread(void* pUserData);
+        /**
+         * Sound Test 2 Helper. Initializes all engine components.
+         **/
+        static void soundTest2Helper();
 
         static ma_thread_result MA_THREADCALL custom_job_thread(void* pUserData);
+
 
         /**
          * data callback for sound engine
